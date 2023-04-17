b2066a05a4a7c3c13990b0cb43f3a9def4e8c182
Matheus Proenca <matheusproencaz@gmail.com>
2023-04-16 23:57:06 -0300

    Finalização do projeto

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,16 +1,16 @@
 import express from 'express';
-
 import './database';
 import 'reflect-metadata';
-
 import ErrorHandler from './services/exceptions/ErrorHandler';
+import cors from 'cors';
 import { routes } from './routes';
+
 
 const app = express();
 const PORT = process.env.APP_PORT;
 
 app.use(express.json());
-
+app.use(cors());
 app.use(routes);
 
 app.use(ErrorHandler);
